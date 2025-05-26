b4c35b6b465ad99441a38405af614a2ee4b32015
joaopedrofariaferreira <joao.pedro.ferreira.profissional@gmail.com>
2025-05-26 19:28:46 -0300

    Adicionado cache config para comunicaçao com o redis

---
+++ src/main/java/com/joaopedrofariaferreira/weatherapi/weather_api/config/CacheConfig.java
@@ -0,0 +1,35 @@
+package com.joaopedrofariaferreira.weatherapi.weather_api.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)) // Tempo de expiração de 10 minutos
+                .prefixCacheNameWith("weather-api::") // Prefixo para as chaves do cache
+                .disableCachingNullValues(); // Desabilita o cache de valores nulos
+
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+
+    @Bean
+    public CacheErrorHandler errorHandler() {
+        return new SimpleCacheErrorHandler();
+    }
+}
